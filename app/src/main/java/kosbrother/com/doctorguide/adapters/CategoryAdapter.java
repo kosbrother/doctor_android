dd022d404bff7d990131dd22f15d41e86d4dfaa5
Steven Ko <chunyuko85@gmail.com>
2015-12-25 19:10:03 +0800

    first push

---
+++ app/src/main/java/kosbrother/com/doctorguide/adapters/CategoryAdapter.java
@@ -0,0 +1,50 @@
+package kosbrother.com.doctorguide.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import kosbrother.com.doctorguide.R;
+
+/**
+ * Created by steven on 12/25/15.
+ */
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>{
+
+    private final LayoutInflater mLayoutInflater;
+    private final Context mContext;
+    private String[] mTitles;
+
+    public CategoryAdapter(Context context,String[] titles) {
+        mTitles = titles;
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public CategoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new CategoryViewHolder(mLayoutInflater.inflate(R.layout.item_category, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(CategoryAdapter.CategoryViewHolder holder, int position) {
+        holder.mTextView.setText(mTitles[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTitles.length;
+    }
+
+    public static class CategoryViewHolder extends RecyclerView.ViewHolder {
+        TextView mTextView;
+
+        CategoryViewHolder(View view) {
+            super(view);
+            mTextView = (TextView)view.findViewById(R.id.text_view);
+        }
+    }
+}
