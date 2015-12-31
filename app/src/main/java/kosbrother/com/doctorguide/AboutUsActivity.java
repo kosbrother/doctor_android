25a30decc2f3424acad1056c8ce10ef9c82081ee
Steven Ko <chunyuko85@gmail.com>
2015-12-31 18:55:49 +0800

    aboutus activity

--- app/src/main/java/kosbrother/com/doctorguide/AboutUsActivity.java
+++ app/src/main/java/kosbrother/com/doctorguide/AboutUsActivity.java
@@ -1,29 +1,23 @@
 package kosbrother.com.doctorguide;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
-public class SettingActivity extends AppCompatActivity {
+public class AboutUsActivity extends AppCompatActivity {
 
     private ActionBar actionbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_about_us);
 
         actionbar = getSupportActionBar();
-        actionbar.setTitle("設定");
+        actionbar.setTitle("");
         actionbar.setDisplayHomeAsUpEnabled(true);
-    }
-
-    public void aboutUsClick(View v) {
-        Intent intent = new Intent(this, AboutUsActivity.class);
-        startActivity(intent);
+        actionbar.setElevation(0);
     }
 
     @Override
