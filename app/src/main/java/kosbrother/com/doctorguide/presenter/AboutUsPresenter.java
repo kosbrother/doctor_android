05f38dbf7b0990f701517c6f3181778135469fb9
atarsdnh <atarsdnh@gmail.com>
2016-02-24 16:40:45 +0800

    Modify AboutUsActivity to MVP pattern with test

---
+++ app/src/main/java/kosbrother/com/doctorguide/presenter/AboutUsPresenter.java
@@ -0,0 +1,24 @@
+package kosbrother.com.doctorguide.presenter;
+
+import kosbrother.com.doctorguide.view.AboutUsView;
+
+public class AboutUsPresenter {
+    private AboutUsView view;
+
+    public AboutUsPresenter(AboutUsView view) {
+        this.view = view;
+    }
+
+    public void onCreate() {
+        view.setContentView();
+        view.initContentView();
+    }
+
+    public void onFeedbackButtonClick() {
+        view.startFeedbackActivity();
+    }
+
+    public void onHomeItemSelected() {
+        view.finish();
+    }
+}
