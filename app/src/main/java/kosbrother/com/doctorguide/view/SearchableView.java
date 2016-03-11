74ed5d89335eab2602b4824857bf5771b99890a4
HenryTung <HenryTung@kosbrother.com>
2016-03-11 13:14:29 +0800

    Modify SearchableActivity to mvp

---
+++ app/src/main/java/kosbrother/com/doctorguide/view/SearchableView.java
@@ -0,0 +1,20 @@
+package kosbrother.com.doctorguide.view;
+
+import java.util.ArrayList;
+
+import kosbrother.com.doctorguide.entity.Doctor;
+import kosbrother.com.doctorguide.entity.Hospital;
+
+public interface SearchableView extends ProgressDialogView{
+    void setContentView();
+
+    void setActionBar();
+
+    void setActionBarTitle(String actionBarTitle);
+
+    void setDoctorListView(ArrayList<Doctor> doctors);
+
+    void setHospitalListView(ArrayList<Hospital> hospitals);
+
+    void startSearchMoreActivity(String type, String queryString);
+}
